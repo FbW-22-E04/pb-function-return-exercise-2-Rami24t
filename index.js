8999c86588a50795055c58286b2bc429a0127bbe
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-18 15:40:19 +0200

    solution

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 // Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
 function vowel_count(str1) {
   //write your code here
+  let vowels = 'AEIOUWaeiouw';
+  numOfVowels = 0;
+  for (i = 0; i < str1.length; i++) {
+      if (vowels.includes(str1[i]))
+          numOfVowels++;
+  }
+  return numOfVowels;
 }
 console.log(vowel_count("The quick brown fox"));
 
@@ -9,6 +16,11 @@ console.log(vowel_count("The quick brown fox"));
 // Write a JavaScript function that generates a string id (specified length) of random characters.
 function makeid(l) {
   //write your code here
+  let id = [];
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < l; i++)
+      id.push(characters[Math.floor(Math.random() * characters.length)]);
+  return id.join('');
 }
 console.log(makeid(8));
 
@@ -16,6 +28,7 @@ console.log(makeid(8));
 // Write a JavaScript program to pass a 'JavaScript function' as parameter.
 function abc() {
   //write your code here
+  return 'abc'
 }
 
-abc();
+console.log(abc());
